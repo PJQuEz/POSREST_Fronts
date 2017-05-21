ee8363bc0c7aa188e93b13cead229775a623c80a
Peerapat Jommanee <QuEz@Peerapats-MacBook-Pro.local>
2017-05-22 00:20:37 +0700

    add new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,19 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@angular/material';
+import {DndModule} from 'ng2-dnd';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { InputDialogComponent } from './dialog/input-dialog/input-dialog.component';
+import { ManageComponent } from './manage/manage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    InputDialogComponent
+    InputDialogComponent,
+    ManageComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +25,8 @@ import { InputDialogComponent } from './dialog/input-dialog/input-dialog.compone
     HttpModule,
     BrowserAnimationsModule,
     MaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DndModule.forRoot()
   ],
   providers: [],
   entryComponents: [
